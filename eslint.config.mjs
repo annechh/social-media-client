8fef37e7b3f57e3411f14254936813c8016b6763
annechh <142426482+annechh@users.noreply.github.com>
2024-10-17 20:37:48 +0200

    Update eslint.config.mjs with configuration data for Cypress tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import jestPlugin from 'eslint-plugin-jest';
+import cypressPlugin from 'eslint-plugin-cypress';
 
 export default [
   {
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
   },
+
   pluginJs.configs.recommended,
+
   {
     files: ['**/*.test.js'],
     languageOptions: {
@@ -22,6 +30,23 @@ export default [
     rules: {
       ...jestPlugin.configs.recommended.rules,
       'jest/prefer-expect-assertions': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.cy.js'],
+    languageOptions: {
+      globals: {
+        ...globals.cypress,
+      },
+    },
+    plugins: {
+      cypress: cypressPlugin,
+    },
+    rules: {
+      ...cypressPlugin.configs.recommended.rules,
+      'cypress/no-unnecessary-waiting': 'off',
+      'no-unused-vars': 'off',
     },
   },
 ];
