0edb279cf2f9508af2bb47d511377c17dbc64bb1
annechh <142426482+annechh@users.noreply.github.com>
2024-10-17 18:01:57 +0200

    Adds unit test for login function to check if token gets stored

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,52 @@
+import { login } from './login.js';
+import { save } from '../../storage/save.js';
+
+jest.mock('../../storage/save.js', () => ({
+  save: jest.fn(),
+}));
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () =>
+      Promise.resolve({
+        accessToken: 'token',
+        user: 'OleBob',
+      }),
+  }),
+);
+
+describe('login function', () => {
+  beforeEach(() => {
+    fetch.mockClear();
+    save.mockClear();
+  });
+
+  it('stores a token', async () => {
+    const email = 'test@stud.noroff.no';
+    const password = 'noroff1234';
+    const accessToken = 'token';
+    const user = { user: 'OleBob' };
+
+    await login(email, password);
+
+    expect(save).toHaveBeenCalledWith('token', accessToken);
+    expect(save).toHaveBeenCalledWith('profile', user);
+  });
+
+  it('fails to store token', async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        ok: false,
+        statusText: 'Bad request',
+      }),
+    );
+
+    const email = 'test@stud.noroff.no';
+    const password = 'wrongPassword';
+
+    await expect(login(email, password)).rejects.toThrow('Bad request');
+
+    expect(save).not.toHaveBeenCalled();
+  });
+});
