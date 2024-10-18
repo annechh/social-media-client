d691d84293f8d8b0701f671932b4eacadfc85a1c
annechh <142426482+annechh@users.noreply.github.com>
2024-10-18 15:29:29 +0200

    Test e2e for login with invalid credentials

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -4,6 +4,10 @@ describe('Authentication', () => {
     cy.wait(500);
   });
 
+  const email = 'testMail@stud.noroff.no';
+  const wrongEmail = 'wrongEmail@stud.noroff.no';
+  const password = 'password1234';
+
   it('Successfully log in user with valid credentials', () => {
     cy.get('#registerForm').find('button[data-auth=login').click();
     cy.get('#loginForm').should('be.visible');
@@ -24,9 +28,24 @@ describe('Authentication', () => {
     cy.get('#loginForm').find('button[type=submit]').click();
     cy.get('button[data-auth=logout]').click();
   });
-});
 
-const user = 'testUser1234';
-const email = 'testMail@stud.noroff.no';
-const wrongEmail = 'wrongEmail@st.noroff.no';
-const password = 'password1234';
+  it('Fails to log in user with invalid credentials', () => {
+    cy.get('#registerForm').find('button[data-auth=login').click();
+    cy.get('#loginForm').should('be.visible');
+    cy.wait(500);
+
+    cy.get('#loginForm').find('input[name=email]').type(wrongEmail);
+    cy.get('#loginForm').find('input[name=password]').type(password);
+    cy.get('#loginForm').find('button[type=submit]').click();
+
+    cy.window().then(() => {
+      expect(localStorage.getItem('token')).to.be.a('string');
+    });
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains(
+        'Either your username was not found or your password is incorrect',
+      );
+    });
+  });
+});
